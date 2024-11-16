a6aa6ce69ba68e7d0c9bc0cabe4abd7809bc96bd
Deepika Srinivasan <deepikasrinivasan2527@gmail.com>
2024-11-16 18:39:52 +0530

    added

---
+++ Pro_three.java
@@ -0,0 +1,21 @@
+package Advanced;
+
+import java.util.Scanner;
+
+public class Pro_three {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
+
